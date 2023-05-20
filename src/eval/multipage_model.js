2e053740b3beb5c648940bb08aca6aea6c0c83cb
Jonathan Kula <jonathan@jdkula.dev>
2023-05-20 15:50:39 -0700

    Add multipage model

---
+++ src/eval/multipage_model.js
@@ -0,0 +1,37 @@
+/** @type {import('./model').WebsiteModel} */
+const model = {
+  start: "A",
+  pages: {
+    A: {
+      elements: [{
+        tag: "a",
+        destination: "B"
+      }, {
+        tag: "a",
+        destination: "#"
+      }, {
+        tag: "a",
+        destination: "#2"
+      }]
+    },
+    B: {
+      elements: [
+        {
+          tag: "input",
+          type: "number",
+          value: undefined,
+        },
+        {
+          tag: "button",
+          onClick: () => {
+            if (model.pages["B"].elements[0].value > 200) {
+              throw new Error("RIP");
+            }
+          },
+        },
+      ],
+    },
+  },
+};
+
+model;
